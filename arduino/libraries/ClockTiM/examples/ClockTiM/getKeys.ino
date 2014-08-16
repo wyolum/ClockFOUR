d934e33c4ce9c974b70458ba5f78f6507e309056
Josef Schneider <chnaideur@gmail.com>
2014-08-16 21:13:22 +1000

    Finally got push-button code clean and working. Implemented routines for settings configuration.

--- arduino/libraries/ClockTiM/examples/ClockTiM/getKeys.ino
+++ arduino/libraries/ClockTiM/examples/ClockTiM/getKeys.ino
@@ -1,22 +1,37 @@
 
+#define DEBOUNCE_TIME	10
+
 struct KeyStates *getKeys() {
 	return getKeys(false, LONG_PRESS_MILLIS);
 }
-
 
 // Returns a byte with the idx of any newly pressed keys set to '1'. Also includes debouncing.
 struct KeyStates *getKeys(boolean repeat, long repeatAfter) {
 	static uint8_t debounce[4] = { 0 };			// Stores the queue of the push button states
 	static uint8_t debounceIdx = 0;				// Stores the index we are current at within the debounce queue
-	static uint8_t states = 0;					// Stores the state of the keys
-	static unsigned long timeInStates = 0;		// Records the amount of time <states> has been in its current state
-	static boolean longPressDetect;
 	
-	static KeyStates keys;
+	static unsigned long lastCheck = 0;			// Checks the time elapsed since last check to make sure that calling frequency is not too high
+	static unsigned long longHoldCheck = 0;		// Sets the limit of time after which we know that the button has been pressed a long time
+	static unsigned long lastRepeat = 0;		// Sets that point in time at which the next repeat can occur
+	static boolean longPressDetect = false;		// True when a long-press has been detected
 	
+	static KeyStates keys = { 0 };
+	static uint8_t held = 0;
+	
+	unsigned long ms;
+	uint8_t prevHeld;							// Temp variable to perform comparison with
 	uint8_t pressed = 0;						// Holds which buttons are currently pressed
-	uint8_t up;									// Buttons that are pressed
-	uint8_t down;								// Buttons that are released
+	
+	keys.justPressed = 0;
+	keys.longPressed = 0;
+	keys.repeated = 0;
+	
+	ms = millis();
+	if((ms - lastCheck) < DEBOUNCE_TIME) {
+		return &keys;
+	}
+	
+	lastCheck = ms;
 	
 	if(digitalRead(BUTTON_L) == LOW) {
 		pressed |= BUTTON_L_IDX;
@@ -29,38 +44,29 @@ struct KeyStates *getKeys(boolean repeat, long repeatAfter) {
 	debounce[debounceIdx++] = pressed;
 	debounceIdx &= 3;
 	
-	down = debounce[0] & debounce[1] & debounce[2] & debounce[3];
-	up = debounce[0] | debounce[1] | debounce[2] | debounce[3];
+	prevHeld = held;
+	held = debounce[0] & debounce[1] & debounce[2] & debounce[3];
 	
-	// Determine which buttons have freshly been pressed
-	uint8_t prevStates = states;
-	states &= up;
-	states |= down;
-	keys.justPressed = states & (states ^ prevStates);
+	// Determine which buttons have just been pressed
+	keys.justPressed = held & (held ^ prevHeld);
 	
-	// Now test which keys have been pressed for a long time
-	keys.longPressed = 0x00;
-	if(states != 0) {
+	if(held != 0 && held == prevHeld) {
 		if(longPressDetect) {
-			// We are already in the middle of a long-press
-			
-			// Don't do anything unless we are repeating
-			if(repeat) {
-				keys.justPressed = up;
-				delay(REPEAT_DELAY);
+			// This conditional is entered every time after longHoldCheck threshold is met
+			if(repeat && (lastRepeat - ms) > REPEAT_DELAY) {
+				lastRepeat += REPEAT_DELAY;
+				keys.repeated = held;
 			}
-			} else if(states != prevStates) {
-			// A button has just been pressed. Mark the time at which this happened
-			timeInStates = millis();
-			longPressDetect = false;
-			} else if(millis() - timeInStates > repeatAfter) {
-			// Enough time has elapsed for a long press - mark it as such
-			keys.longPressed = up;
+		} else if((ms - longHoldCheck) > repeatAfter) {
+			// This conditional is entered the first time the longHoldCheck threshold is met
+			keys.longPressed = held;
 			longPressDetect = true;
+			lastRepeat = ms;
 		}
-		} else {
+	} else {
 		longPressDetect = false;
+		longHoldCheck = ms;
 	}
-	
+
 	return &keys;
 }
--- arduino/libraries/ClockTiM/examples/ClockTiM/settings.ino
+++ arduino/libraries/ClockTiM/examples/ClockTiM/settings.ino
@@ -1,22 +1,12 @@
 
-typedef union Settings {
-	// WARNING: array has to be at least as large as there are elements in the struct
-	uint8_t array[3];
-	struct {
-		uint8_t useGPS;
-		uint8_t useDegF;
-		uint8_t colour;
-	};
-};
-
 Settings clockSettings;
 
 void saveSettings() {
-	// Saves settings to EEPROM
+	// Save settings to EEPROM
 }
 
 
 void loadSettings() {
-	// Saves the settings to EEPROM
+	// Load the settings from EEPROM
 }
 
