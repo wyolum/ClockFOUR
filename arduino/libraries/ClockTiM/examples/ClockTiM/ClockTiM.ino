2e06baace4aea37bb6ff3d30fd9cd25426376829
Josef Schneider <chnaideur@gmail.com>
2014-08-16 12:14:22 +1000

    Fixed the getKeys() function so it returns recently pressed keys, long pressed keys and also has repeat after long-press functionality

--- arduino/libraries/ClockTiM/examples/ClockTiM/ClockTiM.ino
+++ arduino/libraries/ClockTiM/examples/ClockTiM/ClockTiM.ino
@@ -16,9 +16,7 @@
 
 typedef struct KeyStates{
 	uint8_t justPressed;	// Buttons that have just been pressed
-	uint8_t longPressed;	// Buttons that have been pressed for a long time
-	uint8_t up;				// Buttons that are pressed
-	uint8_t down;			// Buttons that are released
+	uint8_t longPressed;	// Buttons that have just been pressed for a long time
 };
 
 void setup() {
@@ -32,7 +30,7 @@ void setup() {
 	digitalWrite(PIN_LOW, LOW);
 	
 	// Start the serial port
-	Serial.begin(9600);
+	Serial.begin(115200);
 	
 	// Initialise the display
 	disp_init();
@@ -46,7 +44,7 @@ void loop() {
 	
 	uint16_t ledStates[8] = { 0 };
 	KeyStates *keys = NULL;
-		
+	
 	keys = getKeys();
 	
 	// Depending on the keys just pressed, take the necessary action
@@ -112,16 +110,23 @@ void displayWord(uint16_t *ledStates, int time) {
 	}
 }
 
-// Returns a byte with the idx of any newly pressed keys set to '1'. Also includes debouncing.
 struct KeyStates *getKeys() {
-	static uint8_t debounce[4];			// Stores the queue of the push button states
-	static uint8_t debounceIdx = 0;		// Stores the index we are current at within the debounce queue
-	static uint8_t states;				// Stores the state of the keys
-	static long timeInStates = 0;		// Records the amount of time <states> has been in its current state
+	return getKeys(false, LONG_PRESS_MILLIS);
+}
+
+// Returns a byte with the idx of any newly pressed keys set to '1'. Also includes debouncing.
+struct KeyStates *getKeys(boolean repeat, long repeatAfter) {
+	static uint8_t debounce[4] = { 0 };			// Stores the queue of the push button states
+	static uint8_t debounceIdx = 0;				// Stores the index we are current at within the debounce queue
+	static uint8_t states = 0;					// Stores the state of the keys
+	static unsigned long timeInStates = 0;		// Records the amount of time <states> has been in its current state
+	static boolean longPressDetect;				
 	
 	static KeyStates keys;
 	
-	uint8_t pressed = 0;				// Holds which buttons are currently pressed
+	uint8_t pressed = 0;						// Holds which buttons are currently pressed
+	uint8_t up;									// Buttons that are pressed
+	uint8_t down;								// Buttons that are released
 	
 	if(digitalRead(BUTTON_L) == LOW) {
 		pressed |= BUTTON_L_IDX;
@@ -134,25 +139,36 @@ struct KeyStates *getKeys() {
 	debounce[debounceIdx++] = pressed;
 	debounceIdx &= 3;
 	
-	keys.down = debounce[0] & debounce[1] & debounce[2] & debounce[3];
-	keys.up = debounce[0] | debounce[1] | debounce[2] | debounce[3];
+	down = debounce[0] & debounce[1] & debounce[2] & debounce[3];
+	up = debounce[0] | debounce[1] | debounce[2] | debounce[3];
 	
-	// Determine which buttons have freshly been pressed and return the result
+	// Determine which buttons have freshly been pressed
 	uint8_t prevStates = states;
-	states &= keys.up;
-	states |= keys.down;
+	states &= up;
+	states |= down;
 	keys.justPressed = states & (states ^ prevStates);
 	
+	// Now test which keys have been pressed for a long time
+	keys.longPressed = 0x00;
 	if(states != 0) {
-		if(states != prevStates) {
-			timeInStates = millis();
-		} else {
-			if(millis() - timeInStates > LONG_PRESS_MILLIS) {
-				keys.longPressed = keys.up;
+		if(longPressDetect) {
+			// We are already in the middle of a long-press
+			
+			// Don't do anything unless we are repeating
+			if(repeat) {
+				keys.justPressed = up;
 			}
+		} else if(states != prevStates) {
+			// A button has just been pressed. Mark the time at which this happened
+			timeInStates = millis();
+			longPressDetect = false;
+		} else if(millis() - timeInStates > repeatAfter) {
+			// Enough time has elapsed for a long press - mark it as such
+			keys.longPressed = up;
+			longPressDetect = true;
 		}
 	} else {
-		keys.longPressed = 0;
+		longPressDetect = false;
 	}
 	
 	return &keys;
