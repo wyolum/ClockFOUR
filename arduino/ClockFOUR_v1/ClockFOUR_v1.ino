29748680f340a302c7587c8235b2cb200cdcc771
dpincus <david@pincus.org>
2014-12-04 16:06:36 +1100

    Created folder for ClockFOUR_v1 prototype boards
    Code location for v1 prototype boards

---
+++ arduino/ClockFOUR_v1/ClockFOUR_v1.ino
@@ -0,0 +1,400 @@
+
+#include <avr/pgmspace.h>
+#include <Wire.h>      
+#include <DS3231.h>
+
+#include "bitmaps.h"
+
+/************* Enable/disable debug mode *************/
+//#define DEBUG
+
+#ifdef DEBUG
+#define PRINT_DEBUG(x)		Serial.print(x)
+#define PRINTLN_DEBUG(x)	Serial.println(x)
+#else
+#define PRINT_DEBUG(x)
+#define PRINTLN_DEBUG(x)
+#endif
+
+/************* Pin settings *************/
+#define MATRIX_PIN			2
+#define BUTTON_L			A1
+#define BUTTON_R			A3
+#define CAP_SENSE			A2
+#define LDR_PIN				A0
+
+
+/************* Some display settings *************/
+#define MATRIX_WIDTH			14
+#define MATRIX_HEIGHT			13
+#define MIN_BRIGHTNESS			50
+#define MAX_BRIGHTNESS			255
+
+/************* Global time variablesrtc *************/
+bool h12, PM;
+DS3231 rtc;
+
+/************* Types of button inputs *************/
+typedef enum EventTypes {
+	NO_EVENT = 0,
+	BL_CLICK,        // Button left click
+	BL_PRESS,        // Button left press
+	BL_REPEAT,       // Button left repeat
+	BR_CLICK,        // Button right click
+	BR_PRESS,        // Button right press
+	BR_REPEAT,       // Button right repeat
+	EVENT_COUNT      // Number of event types in the queue
+};
+
+#define DEFAULT_REPEAT_DELAY		150      // number of milliseconds between each repeated button press (when held down)
+
+/************* Colour modes *************/
+typedef enum ColourModes {
+	CM_ALL_WHITE = 0,
+	CM_SOLID_COLOUR,
+	CM_FADE,
+	CM_RAINBOW,
+	CM_PARTY,
+	COLOUR_MODE_COUNT
+};
+
+
+/************* Display mode definitions *************/
+typedef enum DisplayModeIdx {
+	DISP_TIME = 0,
+	DISP_SECONDS,
+	DISP_TEMP,
+	MODE_COUNT
+};
+
+uint8_t fade[MODE_COUNT] = {
+	20,		// Time mode = fast fade
+	40,		// Seconds mode = very fast fade
+	0		// Temperature mode = no fade
+};
+
+typedef boolean (*DisplayModeFunc)(void);
+
+DisplayModeFunc displayModes[MODE_COUNT] = {
+	displayTime,
+	displaySeconds,
+	displayTemp
+};
+
+
+/************* Settings struct definition *************/
+typedef union Settings {
+	// WARNING: array has to be at least as large as the number of elements in the struct
+	uint8_t array[9];
+	struct {
+		uint8_t useGPS;
+		uint8_t useDegF;					// 0 if degrees C, 1 if degrees F
+		uint8_t displayMode;				// Stores the display mode we are currently in
+		uint8_t colourModes[MODE_COUNT];	// Each display mode has its own colour mode
+		uint8_t colourVal[MODE_COUNT];		// Each display also has its own colour value
+	};
+};
+extern Settings clockSettings;
+
+
+/**** Main Code ****/
+
+void setup() {
+	// Start the serial port
+	Serial.begin(9600);
+
+	// Start one wire
+	Wire.begin();
+
+	// Initialise the board inputs and outputs
+	pinMode(MATRIX_PIN, OUTPUT);   // declare the ledPin as an OUTPUT:
+	pinMode(LDR_PIN, INPUT);       // declare the LDRPin as an INPUT:
+	
+	// Initialise the display
+	disp_init();
+	
+	// Load the settings from the EEPROM
+	loadSettings();
+	
+	// Initialise the buttons
+	buttonsInit();
+
+	// display a welcome message
+	disp_setBrightness();
+	disp_ScrollWords("Clock4", -40, 1);
+		
+	// enter self test mode if a button has been held down
+	if (popEvent() != NO_EVENT) {
+		self_test(); 
+	}
+}
+
+
+void loop() {	
+	// p_colourMode stores a pointer to the current colour mode
+	uint8_t *p_colourMode = &clockSettings.colourModes[clockSettings.displayMode];
+	uint8_t *p_colourValue = &clockSettings.colourVal[clockSettings.displayMode];
+	
+	buttonsTick();
+	
+	// Depending on the buttons pressed, take the necessary action
+	switch(popEvent()) {
+	case BL_CLICK:
+		clockSettings.displayMode++;
+		if(clockSettings.displayMode >= MODE_COUNT) {
+			clockSettings.displayMode = 0;
+		}
+		
+		clearBufferHistory();
+		
+		// Update p_colourMode to the new display mode
+		p_colourMode = &clockSettings.colourModes[clockSettings.displayMode];
+		p_colourValue = &clockSettings.colourVal[clockSettings.displayMode];
+		
+		PRINT_DEBUG("Switched to display mode: ");
+		PRINTLN_DEBUG(clockSettings.displayMode);
+		
+		saveSettings();
+		break;
+		
+	case BR_CLICK:
+		// Perhaps enter colour settings mode?
+		
+		PRINT_DEBUG("Current colour mode: ");
+		PRINTLN_DEBUG(*p_colourMode);
+
+		(*p_colourMode)++;
+		if(*p_colourMode >= COLOUR_MODE_COUNT) {
+			*p_colourMode = 0;
+		}
+		
+		PRINT_DEBUG("Switched to colour mode:");
+		PRINTLN_DEBUG(*p_colourMode);
+		
+		saveSettings();
+		break;
+		
+	case BR_PRESS:
+		// Pick the colour
+		*p_colourValue = colourConfig(*p_colourValue);
+		*p_colourMode = CM_SOLID_COLOUR;
+		clearBufferHistory();
+		saveSettings();
+		break;
+		
+	case BL_PRESS:
+		// Configure the clock
+		clockConfig();
+		break;
+		
+	default:
+		break;
+	}
+	
+	// Set the display brightness from the LDR
+	disp_setBrightness();
+
+	// First clear all the LEDs, and then depending on the mode call its display function
+	pixBuffer_clear();
+	displayModes[clockSettings.displayMode]();
+	
+	// Figure out which colour mode we are using and then display the pixels
+	disp_refresh(*p_colourMode, *p_colourValue, fade[clockSettings.displayMode]);
+}
+
+
+boolean displayTime() {
+	// Sets and clears all pixels to show the current time in letters
+	uint16_t totalMinutes = rtc.getHour(h12, PM) * 60 + rtc.getMinute();
+	loadTime(totalMinutes);
+}
+
+
+boolean displaySeconds() {
+	// Sets and clear all pixels to show the current seconds count
+	pixBuffer_loadVal(rtc.getSecond(), 0);  // put a "2" in the last place to activate the minute slider
+}
+
+
+boolean displayTemp() {
+	// Gets the temperature from the RTC and loads it into pixel buffer it in degrees C or F depending on settings
+	float celsius = rtc.getTemperature();
+
+	if (clockSettings.useDegF) {
+		float fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+		pixBuffer_loadVal(fahrenheit, 1);
+	} else {
+		pixBuffer_loadVal(celsius, 1);
+	}
+}
+
+uint8_t changeSetting(uint8_t origValue, uint8_t minimum, uint8_t maximum, uint16_t repeatDelay, void (*dispFunc)(uint8_t), uint16_t timeout = 0) {
+	uint8_t value = origValue;
+	long lastRepeat = millis();
+	
+	while(true) {
+		buttonsTick();
+		
+		switch(popEvent()) {
+			
+		case BL_CLICK:
+		case BL_PRESS:
+			return value;
+			
+		case BR_CLICK:
+		case BR_PRESS:
+		case BR_REPEAT:
+			if(millis() - lastRepeat < repeatDelay) {
+				break;
+			}
+			
+			lastRepeat = millis();
+			
+			value++;
+			if(value > maximum) {
+				value = minimum;
+			}
+			break;
+			
+		default:
+			break;
+		}
+		
+		if(timeout != 0 && lastRepeat + timeout < millis()) {
+			return value;
+		}
+	
+		dispFunc(value);
+	}
+	return value;
+}
+
+
+void colourWheelDisp(uint8_t colourMode) {
+	// Configuration display function for the colour picker
+	disp_refresh(CM_SOLID_COLOUR, colourMode, 0);
+}
+
+
+uint8_t colourConfig(uint8_t colourMode) {
+	// Config function for the colour mode
+	pixBuffer_clear();
+	pixBuffer_loadBitmap(Circle_bw_bmp);
+	
+	return changeSetting(colourMode, 0, 255, 50, colourWheelDisp, 2000);
+}
+
+
+/************* Defines the different configuration modes *************/
+typedef enum ConfigMode {
+	GPS = 0,
+	HOUR,
+	MINUTE,
+	TEMP_CF,
+	LAST_MODE
+};
+
+
+// A bit of a hack - if we skip setting the time (because it is set through GPS)
+// we jump to skip time. Not very elegant but will do for now
+#define SKIP_TIME	MINUTE
+
+void clockConfig() {
+	
+	uint8_t mode;
+
+	// Don't give the GPS option if none is attached
+	if(gpsPresent()) {
+		mode = GPS;
+	} else {
+		mode = HOUR;
+		clockSettings.useGPS = 0;
+	}
+	
+	disp_showBWBitmap(Set_bw_bmp, 0x00FFFFFF, 0x00000000);
+	waitDelayOrButton(1000);
+	
+	// Loop through all the configuration modes
+	while(mode != LAST_MODE) {
+		switch(mode) {
+		case GPS:
+			{
+				disp_ScrollWords("GPS:", -15, 3);
+				
+				PRINTLN_DEBUG("Now entering GPS value");
+				clockSettings.useGPS = changeSetting(clockSettings.useGPS, 0, 1, DEFAULT_REPEAT_DELAY, exampleDisplayFunction);
+				if(clockSettings.useGPS) {
+					mode = SKIP_TIME;
+				}
+			}
+			break;
+			
+		case HOUR:
+			{
+				disp_showBWBitmap(Hour_bw_bmp, 0x00FFFFFF, 0x00000000);	// White on black
+				waitDelayOrButton(2000);
+				
+				PRINTLN_DEBUG("Now entering hour value");
+				rtc.setHour(changeSetting(rtc.getHour(h12, PM), 0, 23, DEFAULT_REPEAT_DELAY, disp_displayVal));
+			}
+			break;
+			
+		case MINUTE:
+			{
+				disp_showBWBitmap(Min_bw_bmp, 0x00FFFFFF, 0x00000000);	// White on black
+				waitDelayOrButton(2000);
+				
+				PRINTLN_DEBUG("Now entering minute value");
+				rtc.setMinute(changeSetting(rtc.getMinute(), 0, 59, DEFAULT_REPEAT_DELAY, disp_displayVal));
+				
+				// Set seconds to 0 for a clean start to the minute
+				rtc.setSecond(0);
+			}
+			break;
+			
+		case TEMP_CF:
+			{
+				disp_showBWBitmap(Temp_bw_bmp, 0x00FFFFFF, 0x00000000);	// White on black
+				waitDelayOrButton(2000);
+
+				PRINTLN_DEBUG("Now entering whether temp should be represented in C (0) or F (1)");
+				clockSettings.useDegF = changeSetting(clockSettings.useDegF, 0, 1, DEFAULT_REPEAT_DELAY, disp_TempCF);
+			}
+			break;
+			
+		default:
+			break;
+		}
+		
+		// Select the next config mode
+		mode++;
+	}
+	
+	saveSettings();
+	PRINTLN_DEBUG("Exiting configuration mode");
+}
+
+
+// Creates a delay loop that exits when either the time given
+// has passed, or one of the buttons has been pressed
+uint8_t waitDelayOrButton(uint16_t delayTime) {
+	unsigned long stopTime = millis() + delayTime;
+	uint8_t retVal = NO_EVENT;
+	
+	while(millis() < stopTime) {
+		buttonsTick();
+		retVal = popEvent();
+		if(retVal == BL_CLICK || retVal == BR_CLICK) {
+			// A click event has happened, quit the loop early
+			break;
+		}
+	}
+	
+	return retVal;
+}
+
+
+void exampleDisplayFunction(uint8_t value) {
+	PRINTLN_DEBUG(value);
+}
+
---
+++ arduino/ClockFOUR_v1/getCapButtons.ino
@@ -0,0 +1,143 @@
+
+#include <CapacitiveSensor.h>
+#include <OneCapSensor.h>
+
+// Queue definitions
+#define QUEUE_SIZE		8
+uint8_t queue[QUEUE_SIZE] = {0};
+int8_t front = -1;
+int8_t rear = -1;
+
+// Initialse all the button related stuff
+CapacitiveSensor cs_BUTTON_L = CapacitiveSensor(CAP_SENSE, BUTTON_L);
+CapacitiveSensor cs_BUTTON_R = CapacitiveSensor(CAP_SENSE, BUTTON_R);
+OneCapSensor oneButton_L = OneCapSensor(&cs_BUTTON_L);
+OneCapSensor oneButton_R = OneCapSensor(&cs_BUTTON_R);
+
+// Sets up all the callback function
+void buttonsInit() {
+	oneButton_L.attachClick(buttonLClick);
+	oneButton_L.attachLongPressStart(buttonLLongPress);
+	oneButton_L.attachDuringLongPress(buttonLRepeat);
+	
+	oneButton_R.attachClick(buttonRClick);
+	oneButton_R.attachLongPressStart(buttonRLongPress);
+	oneButton_R.attachDuringLongPress(buttonRRepeat);
+}
+
+// Tick both buttons
+void buttonsTick() {
+	oneButton_L.tick();
+	oneButton_R.tick();
+}
+
+// One function for every event type that can be detected
+void buttonLClick() {
+	addToQeue(BL_CLICK);
+}
+
+void buttonLLongPress() {
+	addToQeue(BL_PRESS);
+}
+
+void buttonLRepeat() {
+	addToQeue(BL_REPEAT);
+}
+
+void buttonRClick() {
+	addToQeue(BR_CLICK);
+}
+
+void buttonRLongPress() {
+	addToQeue(BR_PRESS);
+}
+
+void buttonRRepeat() {
+	addToQeue(BR_REPEAT);
+}
+
+// Returns true if both buttons have been pressed for a long time
+boolean bothLongPressed() {
+	return oneButton_L.isLongPressed() && oneButton_R.isLongPressed();
+}
+
+// Blocks until both buttons have been released from a long press
+void waitWhilePressed() {
+	while(oneButton_L.isPressed() || oneButton_R.isPressed()) {
+		// Loop until both buttons are released
+		buttonsTick();
+	}
+	
+	clearQueue();
+}
+
+// Pushes a byte onto the queue
+void addToQeue(uint8_t value) {
+	
+//	noInterrupts();
+	
+	// If front is just behind rear we have a full queue
+	if((front == 0 && rear == QUEUE_SIZE - 1) || front == rear + 1) {
+		// Queue is full
+		return;
+	}
+	
+	if(front == -1) {
+		// The queue is empty, start using real indices
+		front = rear = 0;
+	} else {
+		// Update the rear index to point to the next empty location
+		if(rear == QUEUE_SIZE - 1) {
+			// We've gone full loop
+			rear = 0;
+		} else {
+			rear++;
+		}
+	}
+	
+	// Store the value on the queue
+	queue[rear] = value;
+	
+//	interrupts();
+}
+
+// Gets a value from the top of the queue
+uint8_t popEvent() {
+	uint8_t retVal;
+	
+//	noInterrupts();
+	
+	// Check if the queue is empty
+	if(front == -1)
+	{
+		// Queue underflow
+		return NO_EVENT;
+	}
+	
+	retVal = queue[front];
+	
+	if(front == rear) {
+		// The last item has been popped. Indicate that the queue is empty.
+		front = rear = -1;
+	} else {
+		// Update the front index to point to the next value in the queue
+		if(front == QUEUE_SIZE - 1) {
+			// We've gone full loop
+			front = 0;
+		} else {
+			front++;
+		}
+	}
+	
+//	interrupts();
+	
+	return retVal;
+}
+
+// Deletes all contents of the queue
+inline void clearQueue() {
+//	noInterrupts();
+	front = -1;
+	rear = -1;
+//	interrupts();
+}
